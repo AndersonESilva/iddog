f344e5d48f7b441e1f765a33e836912dfbbb8b8f
AndersonESilva <andersoneraldodasilva@live.com>
2020-06-26 17:50:03 -0300

    Inicialização do filtro

---
+++ app/src/main/java/br/com/anderson/iddog/data/enum/CategoryEnum.kt
@@ -0,0 +1,11 @@
+package br.com.anderson.iddog.data.enum
+
+/**
+ * Created by anderson on 26/06/2020.
+ */
+enum class CategoryEnum(val category: String) {
+    HUSKY("husky"),
+    HOUND("hound"),
+    PUG("pug"),
+    LABRADOR("labrador")
+}
--- app/src/main/java/br/com/anderson/iddog/feature/feed/activity/FeedActivity.kt
+++ app/src/main/java/br/com/anderson/iddog/feature/feed/activity/FeedActivity.kt
@@ -1,14 +1,19 @@
 package br.com.anderson.iddog.feature.feed.activity
 
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import br.com.anderson.iddog.R
+import br.com.anderson.iddog.data.enum.CategoryEnum
 import br.com.anderson.iddog.databinding.ActivityFeedBinding
 import br.com.anderson.iddog.feature.base.BaseActivity
 import br.com.anderson.iddog.feature.feed.viewmodel.FeedViewModel
+import java.util.*
 
 /**
  * Created by anderson on 25/06/2020.
  */
-class FeedActivity : BaseActivity<ActivityFeedBinding, FeedViewModel>() {
+class FeedActivity : BaseActivity<ActivityFeedBinding, FeedViewModel>(){
 
     override fun getLayoutId(): Int = R.layout.activity_feed
 
@@ -16,5 +21,25 @@ class FeedActivity : BaseActivity<ActivityFeedBinding, FeedViewModel>() {
 
     override fun init() {
         bind.viewModel = viewModel
+
+        initFilter()
+    }
+
+    private fun initFilter(){
+        val adapter = ArrayAdapter<CategoryEnum>(this, android.R.layout.simple_spinner_item, CategoryEnum.values())
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        bind.spinnerCategory.adapter = adapter
+
+        bind.spinnerCategory.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                //CategoryEnum.values()[position].category
+            }
+
+        }
     }
 }
