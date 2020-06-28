4ca7904af8201fb4d05db32ce313c9c4dbcd061e
AndersonESilva <andersoneraldodasilva@live.com>
2020-06-28 06:55:15 -0300

    Organização de código

--- app/src/main/java/br/com/anderson/iddog/feature/login/activity/LoginActivity.kt
+++ app/src/main/java/br/com/anderson/iddog/feature/login/activity/LoginActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.view.View
 import androidx.lifecycle.Observer
 import br.com.anderson.iddog.R
+import br.com.anderson.iddog.data.response.UserResponse
 import br.com.anderson.iddog.databinding.ActivityLoginBinding
 import br.com.anderson.iddog.feature.base.BaseActivity
 import br.com.anderson.iddog.feature.feed.activity.FeedActivity
@@ -25,23 +26,34 @@ class LoginActivity : BaseActivity<ActivityLoginBinding, LoginViewModel>() {
 
         bind.btnLogin.setOnClickListener{
             bind.progressBar.visibility = View.VISIBLE
-            if(bind.editEmail.text.toString().validateEmailFormat()){
-                viewModel.signup(bind.editEmail.text.toString()).observe(this, Observer {
-                    if(it.data?.user != null){
-                        val intent = Intent(this, FeedActivity::class.java)
-                        intent.putExtra(Constants.INTENT_USER, it.data.user)
-                        startActivity(intent)
-                        bind.textError.visibility = View.INVISIBLE
-                    }else{
-                        showError()
-                    }
-                    bind.progressBar.visibility = View.INVISIBLE
-                })
-            }else{
-                bind.textError.visibility = View.VISIBLE
-                bind.progressBar.visibility = View.INVISIBLE
-                bind.textError.text = getString(R.string.login_email_invalido)
-            }
+            clickSignup()
+            bind.progressBar.visibility = View.INVISIBLE
         }
+    }
+
+    private fun clickSignup(){
+        if(bind.editEmail.text.toString().validateEmailFormat()){
+            viewModel.signup(bind.editEmail.text.toString()).observe(this, Observer {
+                if(it.data?.user != null){
+                    showFeed(it.data.user)
+                }else{
+                    showError()
+                }
+            })
+        }else{
+            showEmailError()
+        }
+    }
+
+    private fun showFeed(user: UserResponse){
+        val intent = Intent(this, FeedActivity::class.java)
+        intent.putExtra(Constants.INTENT_USER, user)
+        startActivity(intent)
+        bind.textError.visibility = View.INVISIBLE
+    }
+
+    private fun showEmailError(){
+        bind.textError.visibility = View.VISIBLE
+        bind.textError.text = getString(R.string.login_email_invalido)
     }
 }
